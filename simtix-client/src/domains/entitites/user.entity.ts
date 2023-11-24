7e2687f5012fd04d8e874b0033ee469e9970619b
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-24 23:38:19 +0700

    fix: prevent password to be returned

--- simtix-client/src/domains/entitites/user.entity.ts
+++ simtix-client/src/domains/entitites/user.entity.ts
@@ -18,7 +18,7 @@ export class User {
   @Column({ type: 'varchar', length: 255 })
   name: string;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, select: false })
   password: string;
 
   @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
