c3258ed81abc0fb4954dd65fdcdcdd03f4be883a
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-24 23:39:08 +0700

    fix: no need to use api versioning

--- simtix-client/src/main.ts
+++ simtix-client/src/main.ts
@@ -4,7 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8000);
 }
