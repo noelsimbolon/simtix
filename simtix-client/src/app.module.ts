b71be693f6f2461fac537426b8251b2d3557dbd0
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-25 23:52:15 +0700

    chore: import queue module

--- simtix-client/src/app.module.ts
+++ simtix-client/src/app.module.ts
@@ -5,11 +5,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeormConfig from './configs/typeorm.config';
 import { UserModule } from './modules/user.module';
+import { QueueModule } from './modules/queue.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeormConfig),
+    QueueModule,
     UserModule,
   ],
   controllers: [AppController],
