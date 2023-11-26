8365b34d24b0b1c9db97a1d012b5ef55f32c5ef6
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-26 16:10:39 +0700

    chore: use IsEmail decorator, change username to email

--- simtix-client/src/domains/dtos/user.dto.ts
+++ simtix-client/src/domains/dtos/user.dto.ts
@@ -1,9 +1,9 @@
-import { IsString, Length, IsOptional } from 'class-validator';
+import { IsString, Length, IsOptional, IsEmail } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
+  @IsEmail()
   @Length(1, 255)
-  username: string;
+  email: string;
 
   @IsString()
   @Length(1, 255)
@@ -18,15 +18,20 @@ export class UpdateUserDto {
   @IsString()
   @IsOptional()
   @Length(1, 255)
-  username?: string;
-
-  @IsString()
-  @IsOptional()
-  @Length(1, 255)
   name?: string;
 
   @IsString()
   @IsOptional()
   @Length(8, 128)
   password?: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  @Length(1, 255)
+  email: string;
+
+  @IsString()
+  @Length(8, 128)
+  password: string;
 }
