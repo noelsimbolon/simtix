fa7b2c63697d669958283afdff37ab79efb8b284
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-26 20:23:35 +0700

    feat: booking module

---
+++ simtix-client/src/modules/booking.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { BookingService } from '../services/booking.service';
+import { BookingController } from '../controllers/booking.controller';
+import { UserModule } from './user.module';
+import { JwtService } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Booking } from '../domains/entitites/booking.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Booking]), UserModule],
+  controllers: [BookingController],
+  providers: [BookingService, JwtService],
+})
+export class BookingModule {}
