3d02e50e1015098b46a6898586b764ca51d1811c
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-27 09:16:34 +0700

    refactor: all caps

--- simtix-client/src/configs/config.ts
+++ simtix-client/src/configs/config.ts
@@ -1,6 +1,6 @@
 export const RABBITMQ_URL =
   process.env.RABBITMQ_URL || 'amqp://simtix-rabbitmq:5672';
 
-export const QUEUE_NAME = 'client_queue';
+export const QUEUE_NAME = 'CLIENT_QUEUE';
 
 export const APP_PORT = process.env.APP_PORT || 8000;
