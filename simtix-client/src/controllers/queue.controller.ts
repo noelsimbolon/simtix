6e9a09c2c192c816ee470285780a8125f0aaa742
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-27 10:14:02 +0700

    chore: change pattern name

--- simtix-client/src/controllers/queue.controller.ts
+++ simtix-client/src/controllers/queue.controller.ts
@@ -3,7 +3,7 @@ import { EventPattern, Payload, Ctx, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class QueueController {
-  @EventPattern('client_queue')
+  @EventPattern('booking_process')
   async consumeMessage(@Payload() data: any, @Ctx() context: RmqContext) {
     console.log(`Received message: ${JSON.stringify(data)}`);
   }
