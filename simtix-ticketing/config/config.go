b0faacd7fcdf6ff77b7c38e9663a85d27529f764
chaerla <chelgchen@gmail.com>
2023-11-29 00:34:04 +0700

    fix: config

--- simtix-ticketing/config/config.go
+++ simtix-ticketing/config/config.go
@@ -1,18 +1,21 @@
 package config
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/pkg/errors"
 	"go.uber.org/fx"
+	"os"
 )
 
 var Module = fx.Module("config", fx.Options(fx.Provide(NewConfig)))
 
 type Config struct {
-	ServiceHost  string `envconfig:"SERVICE_HOST" required:"true"`
-	ServiceState int    `envconfig:"SERVICE_STATE" required:"true" default:"0"`
-	ServiceName  string `envconfig:"SERVICE_NAME" required:"true"`
-	ServicePort  int    `envconfig:"SERVICE_PORT" default:"8000" required:"true"`
+	ServiceHost     string `envconfig:"SERVICE_HOST" required:"true"`
+	ServiceState    int    `envconfig:"SERVICE_STATE" required:"true" default:"0"`
+	ServiceName     string `envconfig:"SERVICE_NAME" required:"true"`
+	ServicePort     int    `envconfig:"SERVICE_PORT" default:"8000" required:"true"`
+	PaymentEndpoint string `envconfig:"PAYMENT_ENDPOINT" required:"true"`
 
 	RedisHost string `envconfig:"REDIS_HOST"`
 	RedisPort string `envconfig:"REDIS_PORT"`
@@ -27,10 +30,25 @@ type Config struct {
 func NewConfig() (*Config, error) {
 	var config Config
 
-	err := envconfig.Process("", &config)
+	filename := os.Getenv("CONFIG_FILE")
 
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to read environment variables.")
+	if filename == "" {
+		filename = ".env"
+	}
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		if err := envconfig.Process("", &config); err != nil {
+			return nil, errors.Wrap(err, "failed to read from env variable")
+		}
+		return &config, nil
+	}
+
+	if err := godotenv.Load(filename); err != nil {
+		return nil, errors.Wrap(err, "failed to read from .env file")
+	}
+
+	if err := envconfig.Process("", &config); err != nil {
+		return nil, errors.Wrap(err, "failed to read from env variable")
 	}
 
 	return &config, nil
